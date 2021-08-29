063087d467134ac6802855f3958666102790dfc3
Starman0620 <>
2021-08-28 20:33:15 -0400

    Add basic QT window

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,10 @@
-#include <iostream>
+#include <QApplication>
+#include "mainwindow.h"
 
-int main()
+int main(int argc, char *argv[])
 {
-	std::cout << "Hello World!" << std::endl;
+	QApplication app(argc, argv);
+	MainWindow mainWindow;
+	mainWindow.show();
+	return app.exec();
 }
